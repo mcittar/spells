324bc42229bd4e2593146ccaad828eb3e2726117
Michael Cittar <mcittar@gmail.com>
2017-05-30 13:33:05 -0700

    Successfully create  basic spell model

--- mysite/spells/models.py
+++ mysite/spells/models.py
@@ -7,3 +7,6 @@ from django.db import models
 class Spell(models.Model):
     name = models.CharField(max_length=200)
     level = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
