fa30402fb7e14b84aa5014fd3afd7d26f8a765c9
Michael Cittar <mcittar@gmail.com>
2017-05-30 14:10:02 -0700

    Add schools

--- mysite/spells/models.py
+++ mysite/spells/models.py
@@ -5,8 +5,32 @@ from django.db import models
 
 # Create your models here.
 class Spell(models.Model):
-    name = models.CharField(max_length=200)
-    level = models.IntegerField(default=0)
+    ABJURATION = 'A'
+    CONJURATION = 'C'
+    DIVINATION = 'D'
+    ENCHANTMENT = 'E'
+    EVOCATION = 'EV'
+    ILLUSION = 'I'
+    NECROMANCY = 'N'
+    TRANSMUTATION = 'T'
+    SPELL_SCHOOL_CHOICES = (
+        (ABJURATION, 'Abjuration'),
+        (CONJURATION, 'Conjuration'),
+        (DIVINATION, 'Divination'),
+        (ENCHANTMENT, 'Enchantment'),
+        (EVOCATION, 'Evocation'),
+        (ILLUSION, 'Illusion'),
+        (NECROMANCY, 'Necromancy'),
+        (TRANSMUTATION, 'Transmutation')
+    )
+    
+    name = models.CharField(max_length = 200)
+    level = models.IntegerField(default = 0)
+    school = models.CharField(
+        max_length = 2,
+        choices = SPELL_SCHOOL_CHOICES,
+        default = EVOCATION,
+    )
 
     def __str__(self):
         return self.name
