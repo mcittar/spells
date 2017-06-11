2a68ec083e3d31d15b0111b81987249147ccb088
Michael Cittar <mcittar@gmail.com>
2017-06-10 20:15:43 -0700

    First test

--- mysite/spells/tests.py
+++ mysite/spells/tests.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+class SchoolTest(TestCase):
+
+    def test_string_representation(self):
+        self.fail("TODO test incomplete")
